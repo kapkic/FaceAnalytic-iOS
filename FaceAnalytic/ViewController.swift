efaa2bb946b6283b26bb96f9805c575a593efdd4
Ahmet Kapkiç (akpkc) <ahmetkapkic@gmail.com>
2018-03-04 14:19:21 +0300

    Init commit

--- FaceAnalytic/ViewController.swift
+++ FaceAnalytic/ViewController.swift
@@ -9,9 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var txtMail : UITextField!
+    @IBOutlet var btnPhoto : UIButton!
+    @IBOutlet var btnSend : UIButton!
+    var photo=false;
+    var mail=false;
+    var switchbut=false;
+   
+    
+    @IBAction func PhotoPress(_ sender: Any) {
+        photo=true;
+        if (mail && photo && switchbut)
+        {
+            
+            btnSend.isEnabled=true;
+        }
+        else
+        {
+            btnSend.isEnabled=false;
+        }
+    }
+    @IBAction func MailEntered(_ sender: Any) {
+        mail=true;
+        if (mail && photo && switchbut)
+        {
+            btnSend.isEnabled=true;
+        }
+        else
+        {
+            btnSend.isEnabled=false;
+        }
+    }
+    @IBAction func Switch(_ sender: Any) {
+        switchbut = !switchbut;
+        if (mail && photo && switchbut)
+        {
+            btnSend.isEnabled=true;
+        }
+        else
+        {
+            btnSend.isEnabled=false;
+        }
+    }
+    @IBAction func StartAnalysis(_ sender: Any) {
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true);
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+         btnSend.isEnabled=false;
+        mail=true;
         // Do any additional setup after loading the view, typically from a nib.
     }
 
