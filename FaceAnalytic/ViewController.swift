2987147afd9f8530ff2c0c50044e7934aa2e4d6b
Ahmet Kapkiç (akpkc) <ahmetkapkic@gmail.com>
2018-03-13 17:12:37 +0300

    That was the thing updated...

--- FaceAnalytic/ViewController.swift
+++ FaceAnalytic/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
     @IBOutlet var txtMail : UITextField!
@@ -53,78 +54,125 @@ class ViewController: UIViewController {
         print ("woahSwitch")
     }
     @IBAction func StartAnalysis(_ sender: Any) {
-        print ("woah")
-        /*inputs
-        input-text name=email type=text
-         tick-mark name=termsofuse value=0 type=checkbox
-         id=imgToUpload name=facephoto type=file
-         */
-        let myUrl = URL(string: "http://www.faceanalytic.com/yukle");
-        var request = URLRequest(url:myUrl!)
-        request.httpMethod = "POST"
-        //let postString = "thesadr@gmail.com";
+        //TODO let's write the fucking history.
         
         
-        //multipart
+        let url = "https://www.faceanalytic.com/yukle" /* your API url */
+        
+        let headers: HTTPHeaders = [
+            /* "Authorization": "your_access_token",  in case you need authorization header */
+            "Content-type": "multipart/form-data"
+        ]
+        
         let parameters = ["email":"thesadr@gmail.com","termsofuse":"1"]
-        let boundary = generateBoundary()
-        guard let mediaImage = Media(withImage: #imageLiteral(resourceName: "testimg"), forKey:"facephoto") else {return}
-        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-        let dataBody = createDataBody(withParameters: parameters, media:[mediaImage], boundary: boundary)
-        request.httpBody = dataBody
-        
-        let session = URLSession.shared
-        session.dataTask(with: request) { (data,response,error) in
-            if let response=response {
-                print (response)
-            }
-            if let data=data {
-                print ("Data is:")
-                print (String(data:data,encoding: String.Encoding.utf8) as String!)
-                do{
-                    let json=try JSONSerialization.jsonObject(with: data, options: [])
-                    print(json)
-                }
-                catch {
-                    print(error)
-                }
-            }
-        }.resume()
-        //
-        
-    }
-    func generateBoundary() -> String {
-        return "Boundary-\(NSUUID().uuidString)"
-    }
-    func createDataBody(withParameters params: Parameters?, media: [Media]?, boundary: String) -> Data {
-        let lineBreak = "\r\n"
-        var body = Data()
-        
-        if let parameters = params {
+        let imageData = UIImageJPEGRepresentation(#imageLiteral(resourceName: "testimg"), 4.0)
+        Alamofire.upload(multipartFormData: { (multipartFormData) in
             for (key, value) in parameters {
-                body.append("--\(boundary + lineBreak)")
-                body.append("Content-Disposition: form-data; name=\"\(key)\"\(lineBreak + lineBreak)")
-                body.append("\(value+lineBreak)")
+                multipartFormData.append("\(value)".data(using: String.Encoding.utf8)!, withName: key as String)
+            }
+            
+            if let data = imageData{
+                multipartFormData.append(data, withName: "facephoto", fileName: "image.jpeg", mimeType: "image/jpeg")
+            }
+            
+        }, usingThreshold: UInt64.init(), to: url, method: .post, headers: headers) { (result) in
+            switch result{
+            case .success(let upload, _, _):
+                upload.responseString { response in
+                    print("Succesfully uploaded")
+                    if let err = response.error{
+                        print ("Error1 is:")
+                        print (err)
+                        print ("Response is:")
+                        print (response)
+                        return
+                    }
+                    print ("Completed")
+                    //print (String(data:err,encoding: String.Encoding.utf8) as String!)
+                }
+            case .failure(let error):
+                print("Error in upload: \(error.localizedDescription)")
+                print ("Error2 is:")
             }
         }
-        if let media=media {
-            for photo in media{
-                body.append("--\(boundary + lineBreak)")
-                body.append("Content-Disposition: form-data; name=\"\(photo.key)\"; filename=\"\(photo.filename)\"\(lineBreak)")
-                body.append("Content-Type: \(photo.mimeType + lineBreak + lineBreak)")
-                body.append(photo.data)
-                body.append(lineBreak)
-            }
-        }
-        body.append("--\(boundary)--\(lineBreak)")
-        return body
+        
+        
+        
+//
+//        print ("woah")
+//        /*inputs
+//        input-text name=email type=text
+//         tick-mark name=termsofuse value=0 type=checkbox
+//         id=imgToUpload name=facephoto type=file
+//         */
+//        let myUrl = URL(string: "http://www.faceanalytic.com/yukle");
+//        var request = URLRequest(url:myUrl!)
+//        request.httpMethod = "POST"
+//        //let postString = "thesadr@gmail.com";
+//
+//
+//        //multipart
+//        let parameters = ["email":"thesadr@gmail.com","termsofuse":"1"]
+//        let boundary = generateBoundary()
+//        guard let mediaImage = Media(withImage: #imageLiteral(resourceName: "testimg"), forKey:"facephoto") else {return}
+//        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+//        let dataBody = createDataBody(withParameters: parameters, media:[mediaImage], boundary: boundary)
+//        request.httpBody = dataBody
+//
+//        let session = URLSession.shared
+//        session.dataTask(with: request) { (data,response,error) in
+//            if let response=response {
+//                print (response)
+//            }
+//            if let data=data {
+//                print ("Data is:")
+//                print (String(data:data,encoding: String.Encoding.utf8) as String!)
+//                do{
+//                    let json=try JSONSerialization.jsonObject(with: data, options: [])
+//                    print(json)
+//                }
+//                catch {
+//                    print(error)
+//                }
+//            }
+//        }.resume()
+//        //
+        
     }
+//
+//    func generateBoundary() -> String {
+//        return "Boundary-\(NSUUID().uuidString)"
+//    }
+//    func createDataBody(withParameters params: Parameters?, media: [Media]?, boundary: String) -> Data {
+//        let lineBreak = "\r\n"
+//        var body = Data()
+//
+//        if let parameters = params {
+//            for (key, value) in parameters {
+//                body.append("--\(boundary + lineBreak)")
+//                body.append("Content-Disposition: form-data; name=\"\(key)\"\(lineBreak + lineBreak)")
+//                body.append("\(value+lineBreak)")
+//            }
+//        }
+//        if let media=media {
+//            for photo in media{
+//                body.append("--\(boundary + lineBreak)")
+//                body.append("Content-Disposition: form-data; name=\"\(photo.key)\"; filename=\"\(photo.filename)\"\(lineBreak)")
+//                body.append("Content-Type: \(photo.mimeType + lineBreak + lineBreak)")
+//                body.append(photo.data)
+//                body.append(lineBreak)
+//            }
+//        }
+//        body.append("--\(boundary)--\(lineBreak)")
+//        return body
+//    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true);
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-         btnSend.isEnabled=false;
+         //btnSend.isEnabled=false;
         mail=true;
         // Do any additional setup after loading the view, typically from a nib.
     }
